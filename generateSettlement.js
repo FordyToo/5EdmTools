35ca68d177098039d506d967521a2e58438eaa7d
Nicholas Cheek <nicholascheekdev@gmail.com>
2019-10-30 23:33:52 -0400

    Update generateSettlement.js
    Added knownFor and currentCalamity arrays

--- generateSettlement.js
+++ generateSettlement.js
@@ -7,6 +7,8 @@ function getSettlement() {
     settlement.raceRelations = randomIndex(raceRelations);
     settlement.rulersStatus = randomIndex(rulersStatus);
     settlement.notableTrait = randomIndex(notableTrait);
+    settlement.knownFor = randomIndex(knownFor);
+    settlement.currentCalamity = randomIndex(currentCalamity);
     return settlement;
 }
 
@@ -31,4 +33,10 @@ var notableTrait = [
     'canals in place of streets', 'massive statue or monument', 'grand temple', 'large fortress', 'verdant parks and orchards', 'river divides town', 'major trade center', 'headquarters of a powerful family or guild', 'population mostly wealthy', 'destitute, rundown', 'awful smell (tanneries, open sewers)', 'center of trade for one specific good', 'site of many battles', 'site of a mythic or magical event', 'important library or archive', 'worship of all gods banned', 'sinister reputation', 'notable library or academy', 'site of important tomb or graveyard', 'built atop ancient ruins'
 ]
 
-console.log(getSettlement());
+var knownFor = [
+    'delicious cuisine', 'rude people', 'greedy merchants', 'artists and writers', 'great hero/savior', 'flowers', 'hordes of beggars', 'tough warriors', 'dark magic', 'decadence', 'piety', 'gambling', 'godlessness', 'education', 'wines', 'high fashion', 'political intrigue', 'powerful guilds', 'strong drink', 'patriotism'    
+]
+
+var currentCalamity = [
+    'suspected vampire infestation', 'new cult seeks converts', 'important figure died (murder suspected)', "war between rival thieves' guilds", 'plague or famine (sparks riots)', 'plague or famine (sparks riots)', 'corrupt officials', 'marauding monsters', 'marauding monsters', 'powerful wizard has moved into town', 'economic depression (trade disrupted)', 'flooding', 'undead stirring in cemetaries', 'prophecy of doom', 'brink of war', 'internal strife (leads to anarchy)', 'besieged by enemies', 'scandal threatens powerful families', 'dungeon discovered (adventurers flock to town)', 'religious sects struggle for power'
+]
