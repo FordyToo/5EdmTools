d124b70789737887360eed7e863cd5914bfbf90d
Nicholas Cheek <nicholascheekdev@gmail.com>
2020-02-19 14:44:39 -0500

    Update generateScrollsAndWands.js

--- generateScrollsAndWands.js
+++ generateScrollsAndWands.js
@@ -3,14 +3,20 @@ function randomIndex(arr) {
     return result
 };
 
-function getScroll(lvl) {
-     let scroll = randomIndex(lvl);
-     return 'Scroll of ' + scroll;   
+function getScroll(lvl, n) {
+    let scrolls = [];
+    for (let i = 0; i < n; i++) {
+        scrolls[i] = 'Scroll of ' + randomIndex(lvl);
+    }
+    return scrolls;   
 };
 
-function getWand(lvl) {
-    let wand = randomIndex(lvl);
-    return 'Wand of ' + wand;
+function getWand(lvl, n) {
+    let wands = [];
+    for (let i = 0; i < n; i++) {
+        wands[i] = 'Wand of ' + randomIndex(lvl);
+    }
+    return wands;
 };
 
 var cantrip = [
@@ -25,5 +31,5 @@ var second = [
     "Aganazzar's Scorcher", 'Aid', 'Alter Self', 'Animal Messenger', 'Arcane Lock', 'Augury', 'Barkskin', 'Beast Sense', 'Blindness/Deafness', 'Blur', 'Branding Smite', 'Calm Emotions', 'Cloud of Daggers', 'Continual Flame', 'Cordon of Arrows', 'Crown of Madness', 'Darkness', 'Darkvision', 'Detect Thoughts', 'Dust Devil', 'Earthbind', 'Enhance Ability', 'Enlarge/Reduce', 'Enthrall', 'Find Steed', 'Find Traps', 'Flame Blade', 'Flaming Sphere', 'Gentle Repose', 'Gust of Wind', 'Heat Metal', 'Hold Person', 'Invisibility', 'Knock', 'Lesser Restoration', 'Levitate', 'Locate Animals or Plants', 'Locate Object', 'Magic Mouth', 'Magic Weapon', "Maximilian's Earthen Grasp", "Melf's Acid Arrow", 'Mirror Image', 'Misty Step', 'Moonbeam', "Nystul's Magic Aura", 'Pass Without Trace', 'Phantasmal Force', 'Prayer of Healing', 'Protection from Poison', 'Pyrotechnics', 'Ray of Enfeeblement', 'Rope Trick', 'Scorching Ray', 'See Invisibility', 'Shatter', 'Silence', 'Skywrite', "Snilloc's Snowball Swarm", 'Spider Climb', 'Spike Growth', 'Spiritual Weapon', 'Suggestion', 'Warding Bond', 'Warding Wind', 'Web', 'Zone of Truth'
 ];
 
-console.log(getScroll(second));
-console.log(getWand(cantrip));
+console.log(getScroll(second, 5));
+console.log(getWand(second, 5));
